8ccc8f0028c8e010dbcb17a02345ae69c7ed499c
jingyi <jingyixj@gmail.com>
2025-01-14 10:20:31 +0100

    Ajuste menor Imagen1Type a ImagenType en el controllador

--- src/Controller/ImagenController.php
+++ src/Controller/ImagenController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Imagen;
-use App\Form\Imagen1Type;
+use App\Form\ImagenType;
 use App\Repository\ImagenRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,7 +26,7 @@ final class ImagenController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $imagen = new Imagen();
-        $form = $this->createForm(Imagen1Type::class, $imagen);
+        $form = $this->createForm(ImagenType::class, $imagen);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -53,7 +53,7 @@ final class ImagenController extends AbstractController
     #[Route('/{id}/edit', name: 'app_imagen_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Imagen $imagen, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(Imagen1Type::class, $imagen);
+        $form = $this->createForm(ImagenType::class, $imagen);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Form/Imagen1Type.php
+++ src/Form/Imagen1Type.php
@@ -4,10 +4,6 @@ namespace App\Form;
 
 use App\Entity\Imagen;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,37 +12,13 @@ class Imagen1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre', TextType::class, [
-                'label' => 'Nombre:',
-                'label_attr' => ['class' => 'etiqueta'],
-            ])
-            ->add('descripcion', TextType::class, [
-                'label' => 'Descripción:',
-                'required' => false,
-                'label_attr' => ['class' => 'etiqueta'],
-            ])
-            ->add('categoria', NumberType::class, [
-                'label' => 'Categoría:',
-                'label_attr' => ['class' => 'etiqueta'],
-                'data' => 1,
-            ])
-            ->add('numVisualizaciones', NumberType::class, [
-                'label' => 'Visualizaciones:',
-                'label_attr' => ['class' => 'etiqueta'],
-            ])
-            ->add('numLikes', NumberType::class, [
-                'label' => 'Likes:',
-                'label_attr' => ['class' => 'etiqueta'],
-            ])
-            ->add('numDownloads', NumberType::class, [
-                'label' => 'Descargas:',
-                'label_attr' => ['class' => 'etiqueta'],
-            ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Password:',
-                'label_attr' => ['class' => 'etiqueta'],
-                'required' => false,
-            ]);
+            ->add('nombre')
+            ->add('descripcion')
+            ->add('categoria')
+            ->add('numVisualizaciones')
+            ->add('numLikes')
+            ->add('numDownloads')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
